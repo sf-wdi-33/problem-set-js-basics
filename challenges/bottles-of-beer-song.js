e19b8d0fc7bb700daaa22ee4cb2c02ed9fce68c5
Diane <yunfeiyue@me.com>
2016-10-26 22:13:47 -0700

    finish the bottles-of-beer-song

--- challenges/bottles-of-beer-song.js
+++ challenges/bottles-of-beer-song.js
@@ -1,24 +1,18 @@
-/*
-
-  Create the "Bottles of beer on the wall" song (watch out for infinite loops!):
-
-  ```
-      5 bottles of beer on the wall,
-      5 bottles of beer!
-      Take one down and pass it around,
-      4 bottles of beer on the wall...
-
-      4 bottles of beer on the wall,
-      4 bottles of beer!
-      Take one down and pass it around,
-      3 bottles of beer on the wall...
-      etc.
-  ```
-
-  Bonuses
-  - How would you change "0" to "No more"?
-  - How would you fix "1 bottles of beer"?
-
-*/
-
-// YOUR CODE HERE
+var bottles;
+var bottleNum = prompt("Please input a number: ");
+for (counter = bottleNum; counter >= 1; counter--)
+{
+    if (counter == 1) {
+        bottles = 'bottle';
+    } else {
+        bottles = 'bottles';
+    }
+    console.log(counter + " " + bottles + " of beer on the wall,");
+    console.log(counter + " " + bottles + " of beer!");
+    console.log("Take one down and pass it aound,");
+    if (counter == 1) {
+        console.log("No more beer on the wall.");
+    } else {
+       console.log(counter-1 + " " + bottles + " of beer on the wall...\n\n");
+    }
+}
